d2c31c9e82d2fc36b2213a75909e1cdc3e3dc58d
BORRA VIVEK SRI <137025500+vivekborra@users.noreply.github.com>
2026-07-04 05:18:44 +0000

    DataTypes

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treates all code as new js version
+
+let name = "Vivek"
+
+let age = 23
+
+let isLoggedIn = true
+
+// string
+// number
+// boolean
+// bigint
+// symbol
+//null
+// undefined 
+
+
+// objects 
+
+
+// type of => tells the data type of a value 
+
+
+console.log(typeof null); // object
+console.log(typeof undefined);
+
