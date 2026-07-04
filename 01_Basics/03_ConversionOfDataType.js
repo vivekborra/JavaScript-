1b5baaa2026be715c8d81877c778e0a6ce42c095
BORRA VIVEK SRI <137025500+vivekborra@users.noreply.github.com>
2026-07-04 05:47:06 +0000

    data conversion

---
+++ 01_Basics/03_ConversionOfDataType.js
@@ -0,0 +1,40 @@
+
+let score = "33";
+
+let NumberofString = Number(score);
+
+console.log(NumberofString);
+
+/* converstion form string to number
+
+     "33aa" => NaN
+     null => 0
+     undefined => NaN
+
+
+*/
+
+/*
+
+    boolean conversion 
+
+    true -> 1, "vivek"
+    false -> 0, ""
+
+
+*/
+
+/* 
+    conversion from any data type to string is possible
+
+*/
+
+let val = null;
+
+let newval = String(val);
+
+console.log(newval);
+
+
+
+
