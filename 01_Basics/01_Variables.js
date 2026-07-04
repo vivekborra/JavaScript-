02807dcafa1fa65bb1d19c253d1bf55c67918235
BORRA VIVEK SRI <137025500+vivekborra@users.noreply.github.com>
2026-07-04 05:03:08 +0000

    Variables in javaScript

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,21 @@
+const accountId = 12345
+
+let accountEmail = "vivek@google.com"
+
+var accountPassword = "123456"
+
+accountCity = "Vizag"
+
+let accountBank; // this gives undefined 
+
+// accountId = 123456 this is not possible as const value cannot be changed  at any cost
+
+// it is suggested not to use var due to block scope and functional scope issues 
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountBank])
+
+
+
+console.log(accountId);
+
